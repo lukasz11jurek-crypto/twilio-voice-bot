c19241c3d219625417a57a8741ac1bb28aa92eed
lukasz11jurek-crypto <lukasz11jurek@gmail.com>
2026-04-07 22:24:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/voice', (req, res) => {
-  res.type('text/xml');
+app.post("/voice", (req, res) => {
+  res.set("Content-Type", "text/xml");
   res.send(`
     <Response>
-      <Say voice="alice">Hello, everything works correctly</Say>
+      <Say>Hello! Everything is working correctly.</Say>
     </Response>
   `);
 });
 
-app.get('/', (req, res) => {
-  res.send('OK');
+app.get("/", (req, res) => {
+  res.send("Server działa");
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Server running on port ' + PORT);
+  console.log("Server running on port " + PORT);
 });
